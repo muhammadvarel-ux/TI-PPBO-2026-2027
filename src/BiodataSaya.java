59eb59bd4bdc37ec96125cbfba3de43b55e4006d
Muhammad Varel <muhammadvarel121@email.com>
2026-09-07 19:26:02 +0700

    menghapus file yang salah push

--- src/BiodataSaya.java
+++
@@ -1,8 +0,0 @@
-public class BiodataSaya {
-    public static void main(String[] args) {
-
-        System.out.print("Nama: Muhammad Varel - ");
-        System.out.print("NIM: 2025573010084\n");
-        System.out.println("Program Studi: Teknik Informatika");
-    }
-}
