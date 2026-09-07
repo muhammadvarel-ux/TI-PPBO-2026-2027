be52e0d2b108221d509f110aaf8344a100183c1d
Muhammad Varel <muhammadvarel121@email.com>
2026-09-07 10:54:37 +0700

    Komentar Kode

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,9 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Nama: ");
-        System.out.println("Muhammad Varel");
-        System.out.println(" ");
-        System.out.println("Ini baris baru");;
+        // Program menampilkan biodata sederhana
+        System.out.println("Nama: Muhammad Varel");
+        /* baris dibawah ini
+        mencetak NIM mahasiswa */
+        System.out.println("NIM: 2025573010084");;
     }
 }
