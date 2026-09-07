1a2e3f955d5dfd1081257abeda6de1b66d6c272e
Muhammad Varel <muhammadvarel121@email.com>
2026-09-07 11:16:36 +0700

    Casting (Konversi Tipe Data)

--- src/VariabelDemo.java
+++ src/VariabelDemo.java
@@ -5,11 +5,18 @@ public class VariabelDemo {
         double tinggi = 165.5;
         char golDarah = '0';
         boolean mahasiswaaktif = true;
+        int nilaibulat = 9;
+        double nilaiduoble = nilaibulat; // widening otomatis
+        double pecahan = 9.8;
+        int hasilcasting = (int) pecahan; // narrowing eksplisit
 
         System.out.println("Nama: " + nama);
         System.out.println("Umur: " + umur);
         System.out.println("Tinggi: " + tinggi);
         System.out.println("Golongan Darah: " + golDarah);
         System.out.println("Aktif : " + mahasiswaaktif);
+        System.out.println("Widening: " + nilaiduoble);
+        System.out.println("Narrowing: " + hasilcasting);
+
     }
 }
