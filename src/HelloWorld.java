2a33a4d4ae27c1f253c4c795d8fed0467234b276
Muhammad Varel <muhammadvarel121@email.com>
2026-09-07 10:36:53 +0700

    Struktur Dasar Program Java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,8 @@
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("Muhammad Varel"); ;
+        System.out.println("Nama: ");
+        System.out.println("Muhammad Varel");
+        System.out.println(" ");
+        System.out.println("Ini baris baru");;
     }
 }
