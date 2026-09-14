c5f3305ccaefb5f8d1db05cccd9ba1136c4ef0cd
Muhammad Varel <muhammadvarel121@email.com>
2026-09-14 10:09:45 +0700

    If-Else

--- praktikum-3/src/IfDemo.java
+++ praktikum-3/src/IfDemo.java
@@ -1,9 +1,11 @@
 public class IfDemo {
-    public static void main (String [] args){
-        int nilai = 80;
+    public static void main(String[] args) {
+        int nilai = 60;
 
-        if (nilai >= 75){
+        if (nilai >= 75) {
             System.out.println("Anda LULUS");
+        } else {
+            System.out.println("Anda Tidak Lulus");
         }
     }
 }
